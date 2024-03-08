aa7a7166dc5ed05d0d0ed1690f63edb36da3305b
Amirreza Jahantab <jahantab83@gmail.com>
2024-03-08 11:11:28 +0330

    build model

--- pdf/models.py
+++ pdf/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone_number = models.CharField(max_length=10)
+    summary = models.TextField(max_length=2000)
+    degree = models.CharField(max_length=200)
+    university = models.CharField(max_length=200)
+    previous_work = models.TextField(max_length=2000)
+    skills = models.TextField(max_length=2000)
+    
